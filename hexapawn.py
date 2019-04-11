c19edc4f615bb0d38991007c90e1df52241f54d1
Defelo <elodef42@gmail.com>
2019-04-11 21:01:41 +0200

    Improve display of board and move options

--- hexapawn.py
+++ hexapawn.py
@@ -9,13 +9,13 @@ def possible_player_moves(state: BOARD) -> List[MOVE]:
     out: List[MOVE] = []
     for i in range(2, 0, -1):
         for j in range(3):
-            if not 0 < state[i][j] < 7:
+            if state[i][j] != 1:
                 continue
-            if 0 < j and state[i - 1][j - 1] == 7:
+            if 0 < j and state[i - 1][j - 1] == 2:
                 out.append(((i, j), (i - 1, j - 1)))
             if state[i - 1][j] == 0:
                 out.append(((i, j), (i - 1, j)))
-            if j < 2 and state[i - 1][j + 1] == 7:
+            if j < 2 and state[i - 1][j + 1] == 2:
                 out.append(((i, j), (i - 1, j + 1)))
     return out
 
@@ -24,13 +24,13 @@ def possible_computer_moves(state: BOARD) -> List[MOVE]:
     out: List[MOVE] = []
     for i in range(2):
         for j in range(3):
-            if state[i][j] != 7:
+            if state[i][j] != 2:
                 continue
-            if 0 < j and 0 < state[i + 1][j - 1] < 7:
+            if 0 < j and state[i + 1][j - 1] == 1:
                 out.append(((i, j), (i + 1, j - 1)))
             if state[i + 1][j] == 0:
                 out.append(((i, j), (i + 1, j)))
-            if j < 2 and 0 < state[i + 1][j + 1] < 7:
+            if j < 2 and state[i + 1][j + 1] == 1:
                 out.append(((i, j), (i + 1, j + 1)))
     return out
 
@@ -42,13 +42,13 @@ def apply_move(state: BOARD, move: MOVE):
 
 
 def check_win(state: BOARD, computers_turn: bool) -> int:
-    if any(x in state[0] for x in (1, 2, 3)):
+    if 1 in state[0]:
         return 1
-    if 7 in state[2]:
+    if 2 in state[2]:
         return 2
-    if not any(7 in l for l in state):
+    if not any(2 in l for l in state):
         return 1
-    if not any(x in l for l in state for x in (1, 2, 3)):
+    if not any(1 in l for l in state):
         return 2
     if computers_turn and not possible_computer_moves(state):
         return 1
@@ -58,15 +58,16 @@ def check_win(state: BOARD, computers_turn: bool) -> int:
 
 
 def stringify_board(state: BOARD) -> List[str]:
-    return [" ".join(str(c) for c in row) for row in state]
+    header = ["+" + "-" * 5 + "+"]
+    return header + ["|" + " ".join(" +#"[c] for c in row) + "|" for row in state] + header
 
 
 def print_board(state: BOARD):
     for row in stringify_board(state):
-        print(row)
+        print("    " + row)
 
 
-def show_possible_moves(state: BOARD) -> List[MOVE]:
+def show_possible_moves(state: BOARD, possible_moves: List[MOVE], numbers: bool, text: str):
     moves: List[str] = stringify_board(state)
     width: int = max(map(len, moves))
     height: int = len(moves)
@@ -74,17 +75,16 @@ def show_possible_moves(state: BOARD) -> List[MOVE]:
     for i in range(height):
         moves[i] = moves[i].ljust(width + 6)
 
-    possible_moves: List[MOVE] = possible_player_moves(state)
     for i, move in enumerate(possible_moves):
         s: BOARD = [[*l] for l in state]
         apply_move(s, move)
         for j, row in enumerate(stringify_board(s)):
-            moves[j] += ["   ", f"{i + 1}) "][j == 0] + row.ljust(width + 4)
+            if numbers:
+                moves[j] += ["   ", f"{i + 1}) "][j == 0]
+            moves[j] += row.ljust(width + 4)
 
-    for row in moves:
-        print(row)
-
-    return possible_moves
+    for i, row in enumerate(moves):
+        print([" " * len(text), text][i == 0] + row)
 
 
 def get_computers_move(state: BOARD) -> MOVE:
@@ -93,12 +93,13 @@ def get_computers_move(state: BOARD) -> MOVE:
 
 def game():
     board = [
-        [7, 7, 7],
+        [2, 2, 2],
         [0, 0, 0],
-        [1, 2, 3]
+        [1, 1, 1]
     ]
     while True:
-        moves: List[MOVE] = show_possible_moves(board)
+        moves: List[MOVE] = possible_player_moves(board)
+        show_possible_moves(board, moves, True, "")
         move: str = input(">> ")
         if move.lower() in ["q", "exit", "quit"]:
             exit()
@@ -111,14 +112,16 @@ def game():
         winner: int = check_win(board, True)
         if winner:
             print_board(board)
-            print(["Player", "Computer"][winner - 1], " wins the game!")
+            print(["Player", "Computer"][winner - 1], "wins the game!")
             break
 
-        apply_move(board, get_computers_move(board))
+        move: MOVE = get_computers_move(board)
+        show_possible_moves(board, [move], False, "Computer:    ")
+        apply_move(board, move)
         winner: int = check_win(board, False)
         if winner:
             print_board(board)
-            print(["Player", "Computer"][winner - 1], " wins the game!")
+            print(["Player", "Computer"][winner - 1], "wins the game!")
             break
 
 
